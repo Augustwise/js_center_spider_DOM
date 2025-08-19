288d08e3f5af7d8ea3ee9f8bf635ce497d63d3db
Mykola <nyr3mvneov@gmail.com>
2025-08-19 21:10:33 +0300

    make build

---
+++ index.b43c28e4.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".spider"),t=document.querySelector(".wall"),c=t.clientWidth,l=t.clientHeight,o=e.offsetWidth,i=e.offsetHeight;e.style.left="".concat(c/2-o/2,"px"),e.style.top="".concat(l/2-i/2,"px");
+//# sourceMappingURL=index.b43c28e4.js.map
